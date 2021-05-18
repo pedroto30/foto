f829220f22bcbb51740f6f3a1e95c80235a572a5
pedroto30 <84160207+pedroto30@users.noreply.github.com>
2021-05-18 12:06:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/foto/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.foto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button bntomar;
+ImageView ivfoto;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bntomar= (Button) findViewById(R.id.bntomar);
+        ivfoto=(ImageView) findViewById(R.id.ivfoto);
+        bntomar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Capturar();
+            }
+        });
+    }
+    public void Capturar ()
+    {
+        Toast.makeText(this,  "comenzando camara", Toast.LENGTH_SHORT).show();
+        Intent i =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i,0);
+    }
+
+    @Override
+    protected void  onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode== Activity.RESULT_OK)
+        {
+            Bundle ext = data.getExtras();
+            Bitmap bmp= (Bitmap)data.getExtras().get("data");
+            ivfoto.setImageBitmap(bmp);
+        }
+    }
+
+
+}
